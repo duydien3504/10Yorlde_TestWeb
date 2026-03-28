394ca9baeb9dc24b446bd9d16dd3b26426ef4ce2
Nguyễn Trường Vinh Sơn <ntvs1707@gmail.com>
2026-03-28 23:56:34 +0700

    feat:add tc_createflashcard

---
+++ Object Repository/Page_CreateFlashcard/Page_EnglishMaster - Nng cao k nng ting Anh ca bn/button_BT U.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_BT U</name>
+   <tag></tag>
+   <elementGuidId>90ef43b2-f744-4941-bda7-42ae68863071</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>.sc-iIvHqT</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' sc-iIvHqT ')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value></value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>0f80adcf-306e-46d8-b644-79f3adcc8131</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sc-iIvHqT dYZfaV</value>
+      <webElementGuid>3100101d-831a-48b3-bf33-d89514d5e2ae</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>BẮT ĐẦU</value>
+      <webElementGuid>13253544-80c8-4a9e-9ec4-753aae11e9a5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-acb50dd114646324c23414e9989b4720</value>
+      <webElementGuid>958791ea-9455-400f-bc3b-7d1a4f7c6bfb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' sc-iIvHqT ')]</value>
+      <webElementGuid>c7dfcb5a-7e3e-4484-b74b-41b286b1f559</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' sc-iIvHqT ')]</value>
+      <webElementGuid>68ac544b-5095-429b-b385-2efdfeec2c35</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = 'BẮT ĐẦU' or . = 'BẮT ĐẦU')]</value>
+      <webElementGuid>32d299f0-3aa7-4c18-94ce-0738e529da07</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_CreateFlashcard/Page_EnglishMaster - Nng cao k nng ting Anh ca bn/textarea_VD_ She accomplished all her goals this.rs
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>textarea_VD_ She accomplished all her goals this</name>
+   <tag></tag>
+   <elementGuidId>e9ee0e8b-7a81-45df-9c78-f4e503a55e96</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>[name=&quot;example&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@name = 'example']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value></value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>textarea</value>
+      <webElementGuid>da7d988d-7a55-492a-a80a-c4640c2c0424</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>example</value>
+      <webElementGuid>1c5ea378-7858-41b8-a1ee-435a2919054b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>VD: She accomplished all her goals this year.</value>
+      <webElementGuid>2ce4e900-cdb1-47e3-a95d-1c533ebc9bba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sc-lixFyN gbWvPQ</value>
+      <webElementGuid>670a9745-6927-4fb1-a645-dd254d48048a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-80e8242184758acd77b9738c60672a65</value>
+      <webElementGuid>5132e334-436c-46f7-8f49-b3b1aa6175ba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@name = 'example']</value>
+      <webElementGuid>546e59f0-280b-4949-a4da-8137078e6315</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@name = 'example']</value>
+      <webElementGuid>2f99a961-6976-4ad6-beed-4c3405ec6daa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//textarea[@name = 'example' and @placeholder = 'VD: She accomplished all her goals this year.']</value>
+      <webElementGuid>8d75116c-22fd-4b13-952f-bb318b2da499</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
